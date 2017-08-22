5e98239e46aa564f811a8c26e68edc4a18ae1302
Admire Musimwa <addymhoti@gmail.com>
2017-08-22 07:50:41 +0200

    added a Bootstrap class called text-justify in the main file and removed the text alignment from main.css

--- app/views/main.php
+++ app/views/main.php
@@ -2,7 +2,7 @@
 <main class="edu">
 	
 	<h3>About</h3>
-	<p><?php echo $username; ?> is a results-driven junior developer with good problem
+	<p class="text-justify"><?php echo $username; ?> is a results-driven junior developer with good problem
 	solving skills. Currently, he is looking for a software development
 	company to join in order to improve his experience in software development. He intends 
 	to use the learned skills to advance his career as
@@ -10,9 +10,9 @@
 		
 	<h3>Education</h3>
 	<img src="../app/images/forthare.jpg" alt="UFH photo" >
-	<p>Admire is a current holder of a honours degree in Computer Science
-	from the university of Fort Hare. He continues to advance his skills 
+	<p class="text-justify">Admire is a current holder of a honours degree in Computer Science
+	from the university of <a href="http://www.ufh.ac.za" target="_blank">Fort Hare</a>. He continues to advance his skills 
 	in programming through online learning platforms such as CodeSchool. Click 
 	<a href="achievements.php">here</a>
-	to view courses he has completed on the CodeSchool website.</p>
+	to view courses he has completed on the <a href="https://www.codeschool.com" target="_blank">CodeSchool</a> website.</p>
 </main>
