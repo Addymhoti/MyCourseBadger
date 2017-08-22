e26909c08e1869c21f3c43166d2b406beb0ffe15
Admire Musimwa <addymhoti@gmail.com>
2017-08-22 09:20:16 +0200

    adjusted the font color of the footer to be all white

--- app/views/main-badger.php
+++ app/views/main-badger.php
@@ -3,7 +3,7 @@
 <?php 
 	foreach($courses as $course){
 		echo '<div class="badge_area">';
-		echo '<img src="' . $course["badge"] .'"/>';
+		echo '<img class="img-respnsive" src="' . $course["badge"] .'"/>';
 		echo '<br>';
 		echo '<a href="' . $course["url"]  .'">' . $course['title'] . '</a>';
 		echo '</div>';
